ba11f59fc28728528881414859bd8ba1245342fe
Skokus <skok2022@gmail.com>
2021-11-16 23:07:12 +0100

    Fixing Map Display

--- WebPage/AzureMapDemo.js
+++ WebPage/AzureMapDemo.js
@@ -27,7 +27,7 @@ function GetMap() {
 
             //Alternatively, use an Azure Maps key. Get an Azure Maps key at https://azure.com/maps. NOTE: The primary key should be used as the key.
             authType: 'subscriptionKey',
-            subscriptionKey: 'Ncnkj0LMbLdo8EVh0p_ucDuxx1WLntCsx0vkKe0MJhw'
+            subscriptionKey: 'tutajKluczMaByć'
         }
     });
 
