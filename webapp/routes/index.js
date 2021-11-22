9dd7d172aff8df234bcce8ff819d32aa535c0b0d
Szymon Kuś <skus30@wp.pl>
2021-11-22 17:32:28 +0100

    subscription key loading from a file

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  fs.readFile('subscription_key.txt', (err, data) => {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.send("Subscription key not found");
+    } 
+    console.log(data.toString());
+    res.render('index', { title: 'Express', subscriptionKey: data.toString() });
+  });
 });
 
 module.exports = router;
